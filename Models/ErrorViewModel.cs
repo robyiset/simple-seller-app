874b3ad9b9e7c5d059ce77d7ec74a95c7719810d
Roby Setiawan <39855052+robyiset@users.noreply.github.com>
2024-11-25 18:09:03 +0700

    add files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace simple_seller_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
